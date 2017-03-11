759ae12f9c6fe14711b97d7a45542d5f0b7aeadb
meelisb <meelis.burget@gmail.com>
2017-03-11 09:42:37 +0200

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by meelis on 11.03.2017.
+ */
+public class Hello {
+        public static void main(String[] args) {
+            System.out.println("Tere, maailm!");
+        }
+    }
+
+
