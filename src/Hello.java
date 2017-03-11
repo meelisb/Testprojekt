cad9f4eb3e0c5ffb5b4c684f07f1bcec7262c64e
meelisb <meelis.burget@gmail.com>
2017-03-11 10:01:38 +0200

    Lisasin tervituse.

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * Created by meelis on 11.03.2017.
+ */
+public class Hello {
+        public static void main(String[] args) {
+            System.out.println("Hi!");
+            System.out.println("Tere, maailm!");
+        }
+    }
+
+
